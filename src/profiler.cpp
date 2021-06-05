e16fad0c93a50e099bf031493cdf24fc9017be35
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-06-05 03:33:59 +0300

    Remove the "Profiling stopped" message

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -1512,7 +1512,6 @@ Error Profiler::runInternal(Arguments& args, std::ostream& out) {
                 if (error) {
                     return error;
                 }
-                out << "Profiling stopped after " << uptime() << " seconds. No dump options specified\n";
                 break;
             }
             // Fall through
